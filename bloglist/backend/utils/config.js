facf0bf5dee204f9f9113862dc4bc2f33fcbf979
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-05-12 20:12:47 +0700

    fix: configure fly.toml for backend deployment

--- bloglist/backend/utils/config.js
+++ bloglist/backend/utils/config.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 
-if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: process.env.BACKEND_SECRETS_FILE })
+/* For loading Docker secrets into Node process */
+// if (process.env.NODE_ENV === 'production') {
+//   require('dotenv').config({ path: process.env.BACKEND_SECRETS_FILE })
+// }
+
+let MONGODB_URI = process.env.MONGODB_URI || undefined
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
-const PORT = process.env.PORT || 3000
-
-const MONGODB_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI
+const PORT = process.env.PORT || 8080
 
 module.exports = { PORT, MONGODB_URI }
