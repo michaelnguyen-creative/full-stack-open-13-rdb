95790286f92350531acc41a35c237cd76c460b85
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-05-18 19:13:20 +0700

    add postgres seed data

--- bloglist/backend/postgres/init.js
+++ bloglist/backend/postgres/init.js
@@ -7,8 +7,6 @@ const testConnectionPostgres = async () => {
   try {
     sequelize.authenticate()
     console.log('Established connection to Postgres at', config.DATABASE_URL)
-    console.log("Synchronizing Postgres models")
-    await sequelize.sync()
   } catch (error) {
     console.log('Postgres connection errror:', error.message)
     return process.exit(1)
