a666aa61cc3d214119b079f958666a125771051c
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-06-05 19:30:01 +0700

    add readingList sequelize model

--- bloglist/backend/postgres/models/readingList.js
+++ bloglist/backend/postgres/models/readingList.js
@@ -1,53 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
-const { sequelize } = require('../init');
+const { Model, DataTypes } = require("sequelize");
+const { sequelize } = require("../init");
 
 class ReadingList extends Model {}
 
-module.exports = ReadingList.init({
+module.exports = ReadingList.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    read: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
     },
     userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     blogId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    }
-}, {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
-    modelName: 'reading_list'
-});
-  
-//   const Blog = sequelize.define('blog', {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       autoIncrement: true,
-//       primaryKey: true
-//     },
-    
-//   });
-  
-//   ReadingList.afterCreate(async (readingList, options) => {
-//     const blog = await Blog.findByPk(readingList.blogId);
-//     if (blog) {
-//       await blog.update({ read: false });
-//     }
-//   });
-
-// Model.define syntax:
-// {
-//     hooks: {
-//         async beforeCreate(readingList) {
-//             const blog = await Blog.findByPk(readingList.blogId);
-//             if (!blog || blog.read) {
-//                 throw new Error('Blog is already read or does not exist');
-//             }
-//         }
-//     }
-// }
+    underscored: true,
+    timestamps: false,
+    modelName: "reading_list",
+  }
+);
