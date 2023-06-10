166c440a3cd28698b9e14e39ced8dc4720c60919
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-06-10 18:59:21 +0700

    housekeeping
    housekeeping

--- fso13-bloglist/backend/controllers/authors.js
+++ fso13-bloglist/backend/controllers/authors.js
@@ -1,16 +1,18 @@
-const { Blog } = require('../postgres/models')
-const { sequelize } = require('../postgres/init')
+const { Blog } = require('../models')
+const { sequelize } = require('../utils/connectPostgres')
 
 const authorsRouter = require('express').Router()
 
-authorsRouter.get('/', async (req, res) => {
+authorsRouter.get('/', async (_req, res) => {
     const authors = await Blog.findAll({
+        // display each author's number of articles and total likes
         attributes: [
             'author',
             [sequelize.fn('count', sequelize.col('id')), 'articles'],
             [sequelize.fn('sum', sequelize.col('likes')), 'likes'],
         ],
         group: ['author'],
+        // sort by total likes in descending order
         order: [[sequelize.fn('sum', sequelize.col('likes')), 'DESC']],
     })
 
